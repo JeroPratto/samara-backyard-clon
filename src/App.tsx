78fbb9e2cae0badbda16fd490607a7ac3eb4884d
Jeronimo Pratto <jeropratto@yahoo.com.ar>
2024-02-15 12:51:52 -0300

    update imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,4 @@
-import {
-	BrowserRouter,
-	Route,
-	Routes,
-	ScrollRestoration,
-	useLocation,
-} from 'react-router-dom'
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom'
 import { Header } from './components/Header'
 import Home from './Pages/Home/Home'
 import { TechSpecs } from './Pages/TechSpecs'
